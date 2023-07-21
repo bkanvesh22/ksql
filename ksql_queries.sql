6df4f3e879a23e5ecf0c26904602cf53432b860e
Anvesh S <bkanvesh22@gmail.com>
2023-07-21 11:39:01 -0500

    Create ksql_queries.sql

---
+++ ksql_queries.sql
@@ -0,0 +1,11 @@
+-- Create a stream with a placeholder for stream name
+CREATE STREAM {stream_name} (...)
+WITH (...);
+
+-- Create a table with a placeholder for table name
+CREATE TABLE {table_name} (...)
+WITH (...);
+
+-- Insert data into the table with a placeholder for table name
+INSERT INTO {table_name} (...)
+SELECT ...;
